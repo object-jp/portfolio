572059d4c778610e3da11878763fa4ec66b77fa2
object <object@seinoutaiganoMacBook-Pro.local>
2022-04-25 16:41:53 +0900

    プロフィール画面の改善

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -18,10 +18,10 @@ const Profile = () => {
       <main>
         <h2>プロフィール</h2>
         <img src='' alt="自分の写真" />
-        <p>名前：正能 大雅</p>
-        <p>フリガナ：ショウノウ タイガ</p>
+        <p>名前：正能 大雅(ショウノウ　タイガ)</p>
         <p>生年月日：2003年06月12日</p>
         <p>出身：神奈川県</p>
+        <p>大学：日本大学文理学部情報科学科</p>
         <p>自己PR</p>
         <hr />
         <IconContext.Provider value={{color: 'gray', size: '25px'}}>
